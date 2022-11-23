984fa7d6dc9ffb6a56521ae61f8e871e80967674
aemrebilaloglu <emrebilaloglu.77@gmail.com>
2022-11-23 22:19:18 +0300

    environment

---
+++ Assets/Scripts/_Managers/UIManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace EKTemplate
+{
+    public class UIManager : MonoBehaviour
+    {
+        //public MainPanel mainPanel;
+
+        #region Singleton
+        public static UIManager instance = null;
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+        }
+        #endregion
+
+        private void Start()
+        {
+            //mainPanel.Active(true);
+            //LevelManager.instance.startEvent.AddListener(StartGame);
+        }
+        public void StartGame()
+        {
+            //mainPanel.ActiveSmooth(false);
+        }
+      
+    }
+}
